7c9683df0d8df6b2401bb9ebdd40fac92d7fbbdd
Вахитов Рустам <therealrustam@yandex.ru>
2022-08-26 12:06:36 +0500

    add db

---
+++ create_db.py
@@ -0,0 +1,43 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect(
+        host="localhost",
+        database="db",
+        user="postgres",
+        password="1ZakonOma")
+
+
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS books;')
+cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+                                 'title varchar (150) NOT NULL,'
+                                 'author varchar (50) NOT NULL,'
+                                 'pages_num integer NOT NULL,'
+                                 'review text,'
+                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                                 )
+
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('A Tale of Two Cities',
+             'Charles Dickens',
+             489,
+             'A great classic!')
+            )
+
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('Anna Karenina',
+             'Leo Tolstoy',
+             864,
+             'Another great classic!')
+            )
+
+conn.commit()
+
+cur.close()
+conn.close()
